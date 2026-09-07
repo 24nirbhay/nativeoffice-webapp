e0f32d51bbd3fdc89e0fc19972eebd58ccd7fdac
nirbhay24 <nirbhaygaude24@gmail.com>
2026-09-07 21:58:34 +0530

    added test for ppt

--- tests/test_ppt.py
+++ tests/test_ppt.py
@@ -1,42 +1,38 @@
-from playwright.sync_api import Page, expect
+import re 
+from playwright.sync_api import Playwright, Page, sync_playwright, expect
 
 
-def test_example(page: Page) -> None:
+def test_ppt(page: Page) -> None:
+
     page.goto("https://tools.nativeoffice.online/")
+    page.get_by_role("button", name="Create new").click()
+    page.get_by_role("button", name="Presentation A widescreen").click()
+    page.get_by_text("Click to add title").click()
+    page.get_by_text("Click to add title").dblclick()
+    page.locator(".sl-editor").fill("hello")
+    page.get_by_text("Click to add subtitle").click()
+    page.get_by_text("Click to add subtitle").dblclick()
+    page.locator(".sl-editor").fill("hello")
+    page.get_by_role("button", name="New slide", exact=True).click()
+    page.get_by_role("button", name="New slide", exact=True).click()
+    page.get_by_role("button", name="New slide", exact=True).click()
+    page.get_by_role("button", name="New slide", exact=True).click()
+    page.locator("div:nth-child(2) > .film-thumb").click(button="right")
+    page.get_by_role("menuitem", name="Delete").click()
 
-    # ==================== OPEN PRESENTATION ====================
+#img insert
+    page.get_by_role("button", name="Image").click()
 
-    page.get_by_role(
-        "button",
-        name="Presentation Design stunning"
-    ).click()
+    expect(
+        page.get_by_role("heading", name="Insert image")
+    ).to_be_visible()
 
-    # ==================== TITLE ====================
+    image_url = page.get_by_role(
+        "textbox",
+        name="Image address",
+        exact=True
+    )
 
-    title = page.get_by_text("Click to add title")
-    expect(title).to_be_visible()
+    image_url.fill("https://pin.it/1E7iVTD3P")
 
-    title.dblclick()
-
-    editor = page.locator(".sl-editor")
-    expect(editor).to_be_visible()
-    expect(editor).to_be_editable()
-
-    editor.fill("hello this is a test")
-    expect(editor).to_contain_text("hello this is a test")
-
-    # ==================== SUBTITLE ====================
-
-    subtitle = page.get_by_text("Click to add subtitle")
-    expect(subtitle).to_be_visible()
-
-    subtitle.dblclick()
-
-    expect(editor).to_be_visible()
-    editor.fill("playwright")
-    expect(editor).to_contain_text("playwright")
-
-    # ==================== SLIDE CANVAS ====================
-
-    canvas = page.locator(".sl-paint")
-    expect(canvas).to_be_visible()
+    page.locator(".dialog-actions .btn-primary").click()
